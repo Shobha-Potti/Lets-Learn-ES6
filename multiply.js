d484f6573a5e977c26daba5122fee05b216bb887
Shobha <shobhapotti123@gmail.com>
2022-04-29 22:53:38 +0530

    Added square function to square on numbers

--- multiply.js
+++ multiply.js
@@ -1,3 +1,8 @@
 let x, y;
 function mul(x, y) { return `The multiplied result is: ${x * y}` }
 console.log(mul(12, 3));
+let a, b;
+let square = function (a) {
+    return `The multiplied result is: ${a ** 2}`
+}
+console.log(square(12));
